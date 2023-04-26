390aa69db8f1c53b16353c746df2b1c09908e1d2
raosahebDmk <raosaheb.deshmukh88@gmail.com>
2023-04-26 15:04:38 +0530

    added config client code

--- src/main/java/com/ms/dao/Hotel.java
+++ src/main/java/com/ms/dao/Hotel.java
@@ -2,6 +2,7 @@ package com.ms.dao;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "hotel")
 public class Hotel {
 	
 	@Id
